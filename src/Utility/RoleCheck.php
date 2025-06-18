e3ec1fb3619bb18cf75513352e71aa3c1a14f22e
Kur3nai <darkserr0@gmail.com>
2025-06-18 15:22:43 +0800

    Update RoleCheck.php

--- src/Utility/RoleCheck.php
+++ src/Utility/RoleCheck.php
@@ -37,33 +37,7 @@ function is_logged_in() : bool {
     return true;
 }
 
-/**
- * Checks if the user's session contains the required session variables for a user that has an active quiz attempt.
- * 
- * Note :
- * 1. This function doesn't call `session_start()`, so you need to call it before calling this function
- * 
- * @return bool true if user has an active quiz attempt, else false
- */
-function has_quiz_attempt() : bool {
-    if(!isset($_SESSION["quiz_attempt"])) {
-        return false;
-    }
 
-    return true;
-}
-
-/**
- * Checks the login status of the user. 
- * This function combines the whole logic of checking if the user is logged in or not.
- * 
- * Note :
- * 1. This function will call `session_start()`
- * 
- * @see has_session() For checking if user submitted a valid php session id cookie
- * @see is_logged_in() For checking if the user session contains the required session variables
- * @return bool true if the user is logged in, else false
- */
 function check_login_status() : bool {
     // Checks if the request contains a php session id cookie first before calling session_start()
     // to avoid creating a new session unintentionally
@@ -77,29 +51,6 @@ function check_login_status() : bool {
         return false;
     }
 
-    if($_SESSION["csrf_token"] !== $_COOKIE["csrf_token"]) {
-        return false;
-    }
-
     return true;
-}
-
-/**
- * Checks if the user is logged in and has an active quiz attempt.
- * This function combines the whole logic of checking if the user has an active quiz attempt or not.
- * 
- * Note :
- * 1. This function will call `session_start()`
- * 
- * @see check_login_status() For checking if the user is logged in or not.
- * @see has_quiz_attempt() For checking if the user session contains the required session variable
- * @return bool true if user has active quiz attempt, else false.
- */
-function check_quiz_attempt_status() : bool {
-    if(!check_login_status()) {
-        return false;
-    }
-
-    return has_quiz_attempt();
 }
 ?>
